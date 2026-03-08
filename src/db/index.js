aff6566f1ec821cfa71787cea9eabca325c30d94
Protyush-Dey <160174897+Protyush-Dey@users.noreply.github.com>
2026-03-08 21:48:13 +0530

    just console url

--- src/db/index.js
+++ src/db/index.js
@@ -3,7 +3,7 @@ import { DB_NAME } from "../constant.js";
 const connectDb = async()=>{
     try {
         const connectionInstance = await mongoose.connect(`${process.env.MONGO_URL}/${DB_NAME}`)
-        console.log(`MongoDB connected: ${connectionInstance.connection.host}`);
+        console.log(`MongoDB connected: ${connectionInstance.connection.name}`);
     } catch (error) {
         console.log(`the error is ${error}`)
         process.exit(1)
