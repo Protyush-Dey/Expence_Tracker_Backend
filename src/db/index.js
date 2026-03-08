547944c2426588a06a48acfedb961d730b189d45
Protyush-Dey <160174897+Protyush-Dey@users.noreply.github.com>
2026-03-08 21:52:11 +0530

    print bug

--- src/db/index.js
+++ src/db/index.js
@@ -3,6 +3,8 @@ import { DB_NAME } from "../constant.js";
 const connectDb = async()=>{
     try {
         const connectionInstance = await mongoose.connect(`${process.env.MONGO_URL}/${DB_NAME}`)
+        console.log(`${process.env.MONGO_URL}/${DB_NAME}`);
+        console.log(`${process.env.MONGO_URL}/${DB_NAME}`);
         console.log(`MongoDB connected: ${connectionInstance.connection.name}`);
     } catch (error) {
         console.log(`the error is ${error}`)
