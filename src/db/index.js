194bc26d2764a95ff4844a5d92f46d7addcbc59a
Protyush-Dey <160174897+Protyush-Dey@users.noreply.github.com>
2026-03-08 21:07:02 +0530

    fixing bugs

--- src/db/index.js
+++ src/db/index.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 import { DB_NAME } from "../constant.js";
 const connectDb = async()=>{
     try {
-        const connectionInstance = mongoose.connect(`${process.env.MONGO_URL}/${DB_NAME}`)
+        const connectionInstance = await mongoose.connect(`${process.env.MONGO_URL}/${DB_NAME}`)
     } catch (error) {
         console.log(`the error is ${error}`)
         process(1)
