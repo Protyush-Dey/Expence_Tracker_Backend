02f7e948cf7f194382fa65cfee70ae02e8245f25
Protyush-Dey <160174897+Protyush-Dey@users.noreply.github.com>
2026-03-12 00:17:02 +0530

    get all expenses with accoutn and delete the account

--- src/Controller/account.controller.js
+++ src/Controller/account.controller.js
@@ -2,7 +2,8 @@ import { asyncHandler } from "../utils/AsyncHandler.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { Account } from "../Models/account.model.js";
-
+import { Expense } from "../Models/expences.model.js";
+import mongoose from "mongoose";
 // create a account
 const createAccount = asyncHandler(async (req, res) => {
   const { account } = req.body;
@@ -68,4 +69,131 @@ const getAllAccountDetails = asyncHandler(async (req, res) => {
     throw new ApiError(401, error?.message || "something went wrong");
   }
 });
-export { createAccount, getAllAccountDetails };
+
+// get expences of a account
+
+const getMonthExpenseOfAccount = asyncHandler(async (req, res) => {
+  const { accountNo } = req.params;
+  if (!accountNo) throw new ApiError(400, "send the account");
+  const account = await Account.findById(accountNo);
+  if (!account) throw new ApiError(400, "account not found");
+  if (account.user.toString() !== req.user.id)
+    throw new ApiError(400, "Access denied");
+  const startOfMonth = new Date();
+  startOfMonth.setDate(1);
+  startOfMonth.setHours(0, 0, 0, 0);
+  const endOfMonth = new Date();
+  endOfMonth.setMonth(endOfMonth.getMonth() + 1);
+  endOfMonth.setDate(0);
+  endOfMonth.setHours(23, 59, 59, 999);
+  const expenses = await Expense.aggregate([
+    {
+      $match: {
+        account: new mongoose.Types.ObjectId(accountNo),
+        date: { $gte: startOfMonth, $lt: endOfMonth },
+      },
+    },
+    {
+      $group: {
+        _id: null,
+        totalSpend: {
+          $sum: {
+            $cond: [{ $eq: ["$isGiven", true] }, "$amount", 0],
+          },
+        },
+        totalGet: {
+          $sum: {
+            $cond: [{ $eq: ["$isGiven", false] }, "$amount", 0],
+          },
+        },
+        expenses: { $push: "$$ROOT" },
+      },
+    },
+  ]);
+  return res.status(200).json(
+    new ApiResponse(
+      200,
+      {
+        accountNo,
+        totalSpend: expenses[0]?.totalSpend || 0,
+        totalGet: expenses[0]?.totalGet || 0,
+        expenses: expenses[0]?.expenses || [],
+      },
+      "Monthly expenses fetched",
+    ),
+  );
+});
+
+//get expense of a account with date
+const getExpenseOfAccountByDates = asyncHandler(async (req, res) => {
+  const { accountNo } = req.params;
+  const { startOfMonth, endOfMonth } = req.query;
+  if (!accountNo) throw new ApiError(400, "send the account");
+  if (!startOfMonth || !endOfMonth) throw new ApiError(400, "send the dates");
+  const startDate = new Date(startOfMonth);
+  const endDate = new Date(endOfMonth);
+  const account = await Account.findById(accountNo);
+  if (!account) throw new ApiError(400, "account not found");
+  if (account.user.toString() !== req.user.id)
+    throw new ApiError(400, "Access denied");
+  const expenses = await Expense.aggregate([
+    {
+      $match: {
+        account: new mongoose.Types.ObjectId(accountNo),
+        date: { $gte: startDate, $lt: endDate },
+      },
+    },
+    {
+      $group: {
+        _id: null,
+        totalSpend: {
+          $sum: {
+            $cond: [{ $eq: ["$isGiven", true] }, "$amount", 0],
+          },
+        },
+        totalGet: {
+          $sum: {
+            $cond: [{ $eq: ["$isGiven", false] }, "$amount", 0],
+          },
+        },
+        expenses: { $push: "$$ROOT" },
+      },
+    },
+  ]);
+  return res.status(200).json(
+    new ApiResponse(
+      200,
+      {
+        accountNo,
+        totalSpend: expenses[0]?.totalSpend || 0,
+        totalGet: expenses[0]?.totalGet || 0,
+        expenses: expenses[0]?.expenses || [],
+      },
+      "Monthly expenses fetched",
+    ),
+  );
+});
+
+//delete account
+const deleteAccount = asyncHandler(async (req, res) => {
+  const { accountNo } = req.params;
+  if (!accountNo) throw new ApiError(400, "send the account");
+  const account = await Account.findById(accountNo);
+  if (!account) throw new ApiError(400, "account not found");
+  if (account.user.toString() !== req.user.id)
+    throw new ApiError(400, "Access denied");
+  const deleteExpenses = await Expense.deleteMany({ account: accountNo });
+  if (!deleteExpenses) throw new ApiError(400, "expenses are not deleted");
+  const deleteaccount = await Account.findByIdAndDelete(accountNo);
+  if (!deleteaccount) throw new ApiError(400, "expenses are not deleted");
+  return res
+    .status(200)
+    .json(new ApiResponse(200, "account deleted successfully"));
+});
+export {
+  createAccount,
+  getAllAccountDetails,
+  getMonthExpenseOfAccount,
+  getExpenseOfAccountByDates,
+  deleteAccount,
+};
--- src/routes/account.route.js
+++ src/routes/account.route.js
@@ -1,8 +1,11 @@
 import { Router } from "express";
 import { verifyJwtToken } from "../middleware/Auth.middleware.js";
-import {createAccount, getAllAccountDetails} from "../Controller/account.controller.js" 
+import {createAccount, getAllAccountDetails, getMonthExpenseOfAccount, getExpenseOfAccountByDates,deleteAccount} from "../Controller/account.controller.js" 
 const router = Router()
 
 router.post("/createAccount" , verifyJwtToken , createAccount)
 router.get("/getAllAccountDetails" , verifyJwtToken , getAllAccountDetails)
+router.get("/getMonthExpenseOfAccount/:accountNo" , verifyJwtToken , getMonthExpenseOfAccount)
+router.get("/getExpenseOfAccountByDates/:accountNo" , verifyJwtToken , getExpenseOfAccountByDates)
+router.delete("/deleteAccount/:accountNo" , verifyJwtToken , deleteAccount)
 export default router
