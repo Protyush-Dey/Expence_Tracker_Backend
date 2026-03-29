7e483dcc89c706cc6935b7dcf7c6e58b322b5359
Protyush <mdey7054@gmail.com>
2026-03-29 18:37:30 +0530

    Update main.route.ts

--- src/Module/main.route.ts
+++ src/Module/main.route.ts
@@ -1,9 +1,15 @@
 import { Application } from "express";
+console.log("==> importing userRouter");
 import userRouter from "./User/user.route";
+console.log("==> importing expenseRouter");
 import expenseRouter from "./Expense/expense.route";
+console.log("==> importing splitRouter");
 import splitRouter from "./Split/split.route";
+console.log("==> importing friendRouter");
 import friendRouter from "./Friend/friend.route";
-import accountRouter from "./Account/account.route"
+console.log("==> importing accountRouter");
+import accountRouter from "./Account/account.route";
+console.log("==> all routers imported");
 
 export default function initializeModules(app: Application): void {
     try {
