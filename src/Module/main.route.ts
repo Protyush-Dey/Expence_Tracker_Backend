a17955d34dc47f04507d3bae5c42bbfb89980f52
Protyush <mdey7054@gmail.com>
2026-03-29 18:33:58 +0530

    alsjdasjdls

--- src/Module/main.route.ts
+++ src/Module/main.route.ts
@@ -6,9 +6,13 @@ import friendRouter from "./Friend/friend.route";
 import accountRouter from "./Account/account.route"
 
 export default function initializeModules(app: Application): void {
-    // app.use("/expTrack/user" , userRouter);
-     app.use("/expTrack/expense" , expenseRouter);
-    // app.use("/expTrack/account" , accountRouter);
-    // app.use("/expTrack/split" , splitRouter);
-    // app.use("/expTrack/friend" , friendRouter);
+    try {
+        app.use("/expTrack/user" , userRouter);
+        app.use("/expTrack/expense" , expenseRouter);
+        app.use("/expTrack/account" , accountRouter);
+        app.use("/expTrack/split" , splitRouter);
+        app.use("/expTrack/friend" , friendRouter);
+    } catch (error) {
+        console.log("main route error" + error)
+    }
 }
