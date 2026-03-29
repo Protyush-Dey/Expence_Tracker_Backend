7bbbd496e47f3a151d11073c4473915a30236a9e
Protyush <mdey7054@gmail.com>
2026-03-29 17:47:33 +0530

    make some fixes

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,8 +1,10 @@
+import dotenv from "dotenv";
+dotenv.config() ;
+
+
 interface Config {
   mongoUrl: String;
 }
-import dotenv from "dotenv";
-dotenv.config() 
 
 export const config: Config = {
   mongoUrl: process.env.MONGO_URL as String,
